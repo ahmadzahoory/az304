5b7b73862a2ea9775296101e07237c1c59d1a352
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-04-15 10:40:15 +0530

    Update lab-304-db-script.sh

--- lab-304-db-script.sh
+++ lab-304-db-script.sh
@@ -9,6 +9,7 @@ apt install -y php libapache2-mod-php php-mysql
 apt install -y curl php-cli php7.2-xml php-mbstring git unzip
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer require guzzlehttp/guzzle
 cd /var/www/html/
 rm index.html
 wget https://raw.githubusercontent.com/ahmadzahoory/az304/master/lab-304-db-code.zip

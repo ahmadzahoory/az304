01d44c6dfcf08d50ca936c7f5a9f817109107186
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-08-18 23:09:10 +0530

    Add files via upload

---
+++ az-304-09-01-script.ps1
@@ -0,0 +1,21 @@
+# Set execution policy
+Set-ExecutionPolicy Unrestricted -Force
+New-Item -ItemType directory -Path 'C:\temp'
+
+# Install IIS and Web Management Tools.
+Import-Module ServerManager
+install-windowsfeature web-server, web-webserver -IncludeAllSubFeature
+install-windowsfeature web-mgmt-tools
+
+# Download the files for our web application.
+Set-Location -Path C:\inetpub\wwwroot
+
+$shell_app = new-object -com shell.application
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ahmadzahoory/az304/master/az-304-09-01-code.zip", (Get-Location).Path + "\az-304-09-01-code.zip")
+
+$zipfile = $shell_app.Namespace((Get-Location).Path + "\az-304-09-01-code.zip")
+$destination = $shell_app.Namespace((Get-Location).Path)
+$destination.copyHere($zipfile.items())
+
+# Create the web app in IIS
+New-WebApplication -Name netapp -PhysicalPath c:\inetpub\wwwroot\logic-app-website -Site "Default Web Site" -force
